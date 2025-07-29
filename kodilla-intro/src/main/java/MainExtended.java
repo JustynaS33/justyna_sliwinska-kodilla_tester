1999d4efcf9a577d4de01f40067b649df910f25b
JustynaS33 <justynag260991@gmail.com>
2025-07-29 21:51:44 +0200

    Dodano klas MainExtended, poprawiono Main i FirstClass, usunito duplikat

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Justyna";
+        int birthYear = 1991;
+        double heightInMeters = 1.69;
+        boolean isMother = (true);
+        char numberOfChildren = '2';
+        boolean isDoughter = (true);
+
+        System.out.println("myName: " + myName);
+        System.out.println("birthYear: " + birthYear);
+        System.out.println("heightInMeters: " + heightInMeters);
+        System.out.println("isMother: " + isMother);
+        System.out.println("numberOfChildren: " + numberOfChildren);
+        System.out.println("isDoughter: " + isDoughter);
+    }
+}
