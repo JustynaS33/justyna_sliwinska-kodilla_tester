5586c4e73048f8c7dbb9a94cc36b8ee79a4971a0
JustynaS33 <justynag260991@gmail.com>
2025-08-07 11:50:50 +0200

    stworzenie klasy SimpleArray z zawartocia tablicy z 5 elementami nazw zwierzat

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,11 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] animals = new String[5];
+        animals[0] = "cat";
+        animals[1] = "dog";
+        animals[2] = "bird";
+        animals[3] = "elephant";
+        animals[4] = "lion";
+
+    }
+}
