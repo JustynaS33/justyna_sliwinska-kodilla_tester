2a82ccbdcf6b6d2292d9f9350424b8c8ccf26e05
JustynaS33 <justynag260991@gmail.com>
2025-09-08 17:50:44 +0200

    Zmodyfikowalam kod z metod checkPrice, checkWeight, checkYear: do jednej metody checkNotebook

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,14 +2,14 @@ public class FirstClass {
     public static void main(String[] args) {
         Notebook notebook = new Notebook(600, 1000);
         System.out.println(notebook.weight + " " + notebook.price);
-        notebook.checkPrice();
+        notebook.checkNotebook();
 
         Notebook heavyNotebook = new Notebook(2000, 2000);
         System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
-        heavyNotebook.checkPrice();
+        heavyNotebook.checkNotebook();
 
         Notebook oldNotebook = new Notebook(1200, 3000);
         System.out.println(oldNotebook.weight + " " + oldNotebook.price);
-        oldNotebook.checkPrice();
+        oldNotebook.checkNotebook();
     }
 }
