b13a2e86c6ff3facc774735c84b78c150e4940a9
Justyna Sliwinska <justynag260991@gmail.com>
2026-06-16 20:54:08 +0200

    FirstClass podstawowy przyklad if/else

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,11 @@
+public class FirstClass {
+    public static void main (String[] args) {
+        System.out.println("Hello from FirstClass!");
+
+        if (1>2) {
+            System.out.println("Niesamowite! Jeden jest większe niź dwa!!!");
+        } else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niz dwa.");
+        }
+    }
+}
