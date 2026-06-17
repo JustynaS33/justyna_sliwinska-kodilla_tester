7c0bdc6f5a0c2416acfd1d0f743c9acd21c6c107
Justyna Sliwinska <justynag260991@gmail.com>
2026-06-17 12:13:10 +0200

    MainExtended- zadanie: zadeklaruj zmienne, wywolaj i wyswietl w konsoli kazda z nich

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String herName = "Xena";
+        System.out.println (herName);
+
+        int herAge = 59;
+        System.out.println(herAge);
+
+        double height = 1.55;
+        System.out.println (height);
+
+        char carClass = 'B';
+        System.out.println (carClass);
+
+        boolean isGoodProgrammer = false;
+        System.out.println (isGoodProgrammer);
+    }
+}
