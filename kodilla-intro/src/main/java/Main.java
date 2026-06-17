ec083147d225eca36ff0593f3d5a5d353cf046b7
Justyna Sliwinska <justynag260991@gmail.com>
2026-06-17 10:52:53 +0200

    Main- zmienne w Javie, deklarowanie zmiennych

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Jola";
+        int myAge = 99;
+        double heigh = 1.68;
+        char carClass = 'B';
+        boolean isGoodProgrammer = false;
+        System.out.println(isGoodProgrammer);
+    }
+}
